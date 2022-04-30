18c49e01611e3b9db354a705cf42581b5b5caf5e
WojtekBogucki <w.bogucki@student.mini.pw.edu.pl>
2022-04-30 18:44:12 +0200

    add functions for saving and loading preprocessed data

--- projects/team_2/preprocessing.py
+++ projects/team_2/preprocessing.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import unicodedata
 import re
+from pathlib import Path
+from typing import Union
 
 
 def normalize_unicode(data: pd.DataFrame) -> pd.DataFrame:
@@ -22,15 +24,32 @@ def drop_long_highlights(data: pd.DataFrame) -> pd.DataFrame:
     highlights_len = data["highlights"].apply(len)
     rows_to_drop = data[highlights_len > 0.5*article_len].index.values
     print(f"Droped {len(rows_to_drop)} rows with too long highlights")
-    return data.drop(rows_to_drop, axis=0).reset_index()
+    return data.drop(rows_to_drop, axis=0).reset_index(drop=True)
 
 
-def preprocess(path: str) -> pd.DataFrame:
-    data = pd.read_csv(path, usecols=[1,2])
+def preprocess(path: Union[str, Path]) -> pd.DataFrame:
+    data = pd.read_csv(path, usecols=[1, 2])
     data = data.drop_duplicates("highlights").reset_index(drop=True)
     data = data.pipe(normalize_unicode)\
                .pipe(remove_noise)\
                .pipe(drop_long_highlights)
     data["article"] = data["article"].apply(lambda x: re.split(r"[.?!;][ ']", x))
     return data
+
+
+def save_data(data_dir: str) -> None:
+    data_dir = Path(data_dir)
+    for subset in ["train", "validation", "test"]:
+        data = preprocess(data_dir / f"{subset}.csv")
+        pkl_path = data_dir / f"{subset}.pkl.zip"
+        data.to_pickle(pkl_path.as_posix())
+        print(f"Saved file {pkl_path}")
+
+
+def load_data(data_dir: str) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    data_dir = Path(data_dir)
+    train = pd.read_pickle(data_dir / "train.pkl.zip")
+    valid = pd.read_pickle(data_dir / "validation.pkl.zip")
+    test = pd.read_pickle(data_dir / "test.pkl.zip")
+    return train, valid, test
 
