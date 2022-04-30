27c3cbd4845e8037abf0e5692529a5c01060a723
Karol Pysiak <karol.pysiak.13@gmail.com>
2022-04-30 14:29:51 +0200

    someting works, or not?

--- projects/team_2/pso.py
+++ projects/team_2/pso.py
@@ -2,32 +2,33 @@ import numpy as np
 
 
 class PSO:
-    def __init__(self, n_particles, similarity_matrix) -> None:
+    def __init__(self, n_particles, similarity_matrix, capacity) -> None:
         self.n_particles = n_particles
         self.similarity_matrix = similarity_matrix
+        self.capacity = capacity
         self.n_dim = self.similarity_matrix.shape[0]
         self.position = np.round(np.random.uniform(0, 1, (self.n_particles, self.n_dim)))
         self.velocity = np.random.uniform(0, 1, (self.n_particles, self.n_dim))
         self.pbest = self.position.copy()
-        self.pbest_score = list(map(PSO.target_function, self.pbest))
+        self.pbest_score = [PSO._target_function(x, self.similarity_matrix, self.capacity) for x in self.pbest]
         tmp_best = np.argmin(self.pbest_score)
         self.gbest = self.position[tmp_best,:].copy()
         self.gbest_score = self.pbest_score[tmp_best]
 
     @staticmethod
-    def target_function(similarity_matrix, x):
+    def _target_function(x, similarity_matrix, capacity):
         x = np.array(x).astype(bool)
         in_var = np.mean(similarity_matrix[x] ** 2)
         out_var = np.mean(similarity_matrix[x, x] ** 2)
-        return out_var - in_var
+        return ((1 - capacity) * out_var) - (capacity * in_var)
 
     @staticmethod
-    def constriction(phi):
+    def _constriction(phi):
         t1 = np.sqrt((phi ** 2) - (4 * phi))
         return 2 / np.abs(2 - phi - t1)
 
-    def update_best(self):
-        scores = list(map(PSO.target_function, self.position))
+    def _update_best(self):
+        scores = [PSO._target_function(x, self.similarity_matrix, self.capacity) for x in self.position]
         for i, score in enumerate(scores):
             if score < self.pbest_score[i]:
                 self.pbest_score[i] = score
@@ -36,7 +37,7 @@ class PSO:
                 self.gbest_score = score
                 self.gbest = self.position[i].copy()
 
-    def update_velocity(self, constriction_coef, nostalgia_coef, social_coef):
+    def _update_velocity(self, constriction_coef, nostalgia_coef, social_coef):
         c1 = np.random.uniform(0, nostalgia_coef, self.position.shape)
         c2 = np.random.uniform(0, social_coef, self.position.shape)
         cognitive = (
@@ -47,13 +48,14 @@ class PSO:
         )
         self.velocity = constriction_coef * (self.velocity + cognitive + social)
 
-    def update_position(self):
+    def _update_position(self):
         sigmed = 1/(1 + np.exp(-self.velocity.copy()))
         rng = np.tile(np.random.uniform(0, 1, self.n_dim), (self.n_particles, 1))
         self.position = np.sign(sigmed-rng) / 2 + .5
 
     def optimize(self, n_iter) -> list[int]:
         for iter in range(n_iter):
-            self.update_best()
-            self.update_velocity(0.7298, 2.05, 2.05)
-            self.update_position()
+            self._update_best()
+            self._update_velocity(0.7298, 2.05, 2.05)
+            self._update_position()
+        return self.gbest
--- projects/team_2/summarization.py
+++ projects/team_2/summarization.py
@@ -1,6 +1,20 @@
-class Summarization:
-    def __init__(self):
-        pass
+from sentence_transformers import SentenceTransformer
+import numpy as np
+from pso import PSO
 
-    def summarize(self, text):
-        pass
+def _cosine(u, v):
+    return np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
+
+def summarize(sentences, capacity=.5, n_iter=5000):  
+    n_sent = len(sentences)
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    sentence_embeddings = model.encode(sentences)
+    similarity_matrix = np.zeros((n_sent, n_sent))
+    for i in range(n_sent):
+        for j in range(i, n_sent):
+            similarity = _cosine(sentence_embeddings[i], sentence_embeddings[j])
+            similarity_matrix[i, j] = similarity
+            similarity_matrix[j, i] = similarity
+    pso = PSO(n_particles=50, similarity_matrix=similarity_matrix, capacity=capacity)
+    optimum = pso.optimize(n_iter)
+    return '. '.join([sentences[i] for i in np.where(optimum==1)[0]])
