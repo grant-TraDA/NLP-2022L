ed1b7e6f4b1e21aef7bca95763e5d595a320884a
WojtekBogucki <w.bogucki@student.mini.pw.edu.pl>
2022-04-30 15:27:58 +0200

    Change method of calculating similarity matrix

--- projects/team_2/summarization.py
+++ projects/team_2/summarization.py
@@ -1,20 +1,13 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
 from pso import PSO
+from scipy.spatial.distance import pdist, squareform
 
-def _cosine(u, v):
-    return np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
 
-def summarize(sentences, capacity=.5, n_iter=5000):  
-    n_sent = len(sentences)
+def summarize(sentences, capacity=.5, n_iter=5000):
     model = SentenceTransformer('all-MiniLM-L6-v2')
     sentence_embeddings = model.encode(sentences)
-    similarity_matrix = np.zeros((n_sent, n_sent))
-    for i in range(n_sent):
-        for j in range(i, n_sent):
-            similarity = _cosine(sentence_embeddings[i], sentence_embeddings[j])
-            similarity_matrix[i, j] = similarity
-            similarity_matrix[j, i] = similarity
+    similarity_matrix = 1 - squareform(pdist(sentence_embeddings, "cosine"))
     print(similarity_matrix)
     pso = PSO(n_particles=50, similarity_matrix=similarity_matrix, capacity=capacity)
     optimum = pso.optimize(n_iter)
