026cf8f37740da64fa31df4707f11972ba5b3def
Karol Pysiak <karol.pysiak.13@gmail.com>
2022-05-08 15:53:51 +0200

    Adding docstring and annotations vol 1

--- projects/team_2/main.py
+++ projects/team_2/main.py
@@ -6,10 +6,12 @@ from sentence_transformers import SentenceTransformer
 
 from preprocessing import load_data
 from summarization import summarize_multiple
+import pandas as pd
+from typing import List
 
 
-def run_and_save(n_iter=250, length=4, capacity=.1, model=SentenceTransformer('all-MiniLM-L6-v2'),
-                 summary_path="summary", targets_path="targets", subset_size=None):
+def run_and_save(n_iter: int=250, length: int=4, capacity: float=.1, model: SentenceTransformer=SentenceTransformer('all-MiniLM-L6-v2'),
+                 summary_path: str="summary", targets_path: str="targets", subset_size: int=None) -> List[str]:
     _, _, test = load_data("data")
 
     if subset_size is None:
@@ -17,17 +19,17 @@ def run_and_save(n_iter=250, length=4, capacity=.1, model=SentenceTransformer('a
     else:
         subset = random.sample(range(len(test)), subset_size)
 
-    summaries = summarize_multiple(test.article[subset], model=model, n_iter=n_iter, length=length, capacity=capacity)
-    target_summaries = test.highlights[subset]
+    summaries: List[str] = summarize_multiple(test.article[subset], model=model, n_iter=n_iter, length=length, capacity=capacity)
+    target_summaries: pd.Series = test.highlights[subset]
 
     for i, s in enumerate(summaries):
-        path = Path(summary_path)
+        path: Path = Path(summary_path)
         os.makedirs(path, exist_ok=True)
         with open(path / (str(i).rjust(5, "0") + ".txt"), "w", encoding="utf-8") as f:
             f.write(s)
 
     for i, s in enumerate(target_summaries):
-        path = Path(targets_path)
+        path: Path = Path(targets_path)
         os.makedirs(path, exist_ok=True)
         with open(path / (str(i).rjust(5, "0") + ".txt"), "w", encoding="utf-8") as f:
             f.write(s)
--- projects/team_2/summarization.py
+++ projects/team_2/summarization.py
@@ -1,33 +1,35 @@
 import numpy as np
 from p_tqdm import p_map
 from scipy.spatial.distance import pdist, squareform
+from sentence_transformers import SentenceTransformer
 
 from pso import PSO
 
 
 class Summarizer(object):
-    def __init__(self, model, n_iter=5000, length=4, capacity=.1):
-        self.model = model
-        self.n_iter = n_iter
-        self.length = length
-        self.capacity = capacity
+    """Helper class allowing multiprocessing summarization runs.
+    """
+    def __init__(self, model: SentenceTransformer, n_iter: int=5000, length: int=4, capacity: float=.1) -> None:
+        self.model: SentenceTransformer = model
+        self.n_iter: int = n_iter
+        self.length: int = length
+        self.capacity: float = capacity
 
-    def __call__(self, article):
+    def __call__(self, article: list[str]) -> str:
         return summarize(article, self.model, n_iter=self.n_iter, length=self.length, capacity=self.capacity)
 
 
-def summarize_multiple(articles, model, n_iter=5000, length=4, capacity=.1):
+def summarize_multiple(articles: list[list[str]], model: SentenceTransformer, n_iter: int=5000, length: int=4, capacity: float=.1) -> list[str]:
     return p_map(Summarizer(model, n_iter=n_iter, length=length, capacity=capacity), articles)
 
 
-def summarize(sentences, model, n_iter=5000, length=4, capacity=.1):
-    sentence_embeddings = model.encode(sentences)
-    article_embedding = model.encode('. '.join(sentences))
-    sm = 1 - squareform(
+def summarize(sentences: list[str], model: SentenceTransformer, n_iter: int=5000, length: int=4, capacity: float=.1) -> str:
+    sentence_embeddings: np.ndarray = model.encode(sentences)
+    article_embedding: np.ndarray = model.encode('. '.join(sentences))
+    sm: np.ndarray = 1 - squareform(
         pdist(np.concatenate((sentence_embeddings, article_embedding.reshape(1, -1)), axis=0), "cosine"))
-    similarity_matrix = sm[:-1, :-1]
-    similarity__to_all = sm[-1, :-1]
-    pso = PSO(n_particles=50, similarities=(similarity_matrix, similarity__to_all), length=length, capacity=capacity)
-    # print(pso.gbest_score)
-    optimum = pso.optimize(n_iter)
+    similarity_matrix: np.ndarray = sm[:-1, :-1]
+    similarity__to_all: np.ndarray = sm[-1, :-1]
+    pso: PSO = PSO(n_particles=50, similarities=(similarity_matrix, similarity__to_all), length=length, capacity=capacity)
+    optimum: np.ndarray = pso.optimize(n_iter)
     return '. '.join([sentences[i] for i in np.where(optimum==1)[0]])
