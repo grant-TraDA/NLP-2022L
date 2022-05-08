3aad93cd6df327f07407bc59d82a381c3698514f
Laura <erdaradun.gaztea+github@gmail.com>
2022-05-08 13:16:43 +0200

    add subset parameter to main

--- projects/team_2/main.py
+++ projects/team_2/main.py
@@ -1,4 +1,5 @@
 import os
+import random
 from pathlib import Path
 
 from sentence_transformers import SentenceTransformer
@@ -8,11 +9,16 @@ from summarization import summarize_multiple
 
 
 def run_and_save(n_iter=250, length=4, capacity=.1, model=SentenceTransformer('all-MiniLM-L6-v2'),
-                 summary_path="summary", targets_path="targets"):
-    train, valid, test = load_data("data")
+                 summary_path="summary", targets_path="targets", subset_size=None):
+    _, _, test = load_data("data")
 
-    summaries = summarize_multiple(test.article, model=model, n_iter=n_iter, length=length, capacity=capacity)
-    target_summaries = test.highlights
+    if subset_size is None:
+        subset = range(len(test))
+    else:
+        subset = random.sample(range(len(test)), subset_size)
+
+    summaries = summarize_multiple(test.article[subset], model=model, n_iter=n_iter, length=length, capacity=capacity)
+    target_summaries = test.highlights[subset]
 
     for i, s in enumerate(summaries):
         path = Path(summary_path)
